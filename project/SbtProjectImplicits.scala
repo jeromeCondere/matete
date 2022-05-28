3486195cc1cab7b20e1ab3739ed6f32e8762405c
alpha-omega <jerome.condere@gmail.com>
2022-05-28 17:38:36 +0100

    minor fix

--- project/SbtProjectImplicits.scala
+++ project/SbtProjectImplicits.scala
@@ -19,18 +19,13 @@ object SbtProjectImplicits {
                 Compile / sourceDirectory := sourceDirectory.value,
                 Compile / scalaSource := scalaSource.value,
                 Compile / mainClass := Some(classname),
-                //Compile / compile := (Config /compile).value,
 
- 
-                // Compile / compile := (Config / Compile / compile).value,              
-                // Compile / compile / mainClass := Some(classname),
-                // Compile  / sources := (Compile /  sources).map(_.filter(_.name == classname.split('.').last + ".scala")).value,
                  mainClass := Some(classname),
                 Compile / run / mainClass := Some(classname),
                 //run / sources ~= (_.filter(_.name == classname.split('.').last + ".scala")),
                 assembly  := (Config / assembly).value,
                 assembly / mainClass := Some(classname),
-                assembly / assemblyJarName:= classname.split('.').last + s"-${version.value}_${scalaVersion.value}",
+                assembly / assemblyJarName:= classname.split('.').last + s"-${version.value}_${scalaVersion.value}.jar",
                 assembly / assemblyMergeStrategy := {
                     case PathList("META-INF", xs @ _*) => MergeStrategy.discard
                     case x if x.endsWith("module-info.class") => MergeStrategy.discard
