69abef887f4f06acfbd75140d89925bceb4827f6
alpha-omega <jerome.condere@gmail.com>
2022-06-25 20:40:47 +0100

    fix simple bug

--- matete/src/main/scala/com/matete/mas/configuration/AgentConfig.scala
+++ matete/src/main/scala/com/matete/mas/configuration/AgentConfig.scala
@@ -1,10 +1,26 @@
 package com.matete.mas.configuration
 
+import org.apache.kafka.common.KafkaFuture.BiConsumer
+
+
+
 case class AgentConfig(
     id: String,
-    producers: List[ProducerConfig]
+    producers: List[ProducerConfig],
+    consumers: List[ConsumerConfig],
+    description: Option[String] = None
+
 )
   
 case class ProducerConfig(
-    name: String
+    keySerializer: String =  "org.apache.kafka.common.serialization.StringSerializer",
+    valueSerializer: String = "org.apache.kafka.common.serialization.StringSerializer",
+    compressionType: String = "snappy"
+)
+
+case class ConsumerConfig(
+    keyDeserializer: String =  "org.apache.kafka.common.serialization.StringDeserializer",
+    valueDeserializer: String = "org.apache.kafka.common.serialization.StringDeserializer",
+    autoCommit: String = "false",
+    groupId: String
 )
